e896d6682dedaf438ef4d63342af23927aabcb0c
qmayo <qmayo2006@gmail.com>
2021-12-18 17:15:45 -0500

    Finished Timer.js with a fork of use-keyboard-timer. TODO: Store times in localstorage instead of state

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,28 +1,59 @@
 import React from 'react';
-import {useState, useEffect, useRef} from 'react';
+import {useState} from 'react';
+import useKeyboardTimer from 'use-keyboard-timer';
+
+
+
+const millisecondsToSeconds = (time) => {
+    return (time / 1000).toFixed(2)
+}
 
 const Timer = () => {
-    const [time, setTime] = useState(0);
-    const [isActive, toggleActive] = useState(false);
-    const interval = useRef(null);
+    const [times, setTimes] = useState([]); //Refactor to pull from localstorage; string because of DNFs
 
-    const startTimer = () => {
-        setTime(0)
-        interval.current = setInterval(() => {
-            setTime((time) => time + 0.01)
-        }, 10)
+    const settings = {
+        timerInput: 'timer',
+        inspection: 'always',
+        timerUpdate: 'deciseconds',
+        timeToRelease: 'stackmat',
+      };
+
+    const keyboardTimerCallback = (time, penalty) => {
+        setTimes([...times, {time: time, penalty: penalty}])
+    }
+  
+    const { time, inspectionTime, state, isTiming, dnf, plusTwo } = useKeyboardTimer(settings, keyboardTimerCallback);
+
+    const renderTime = (time) => {
+        return time.penalty 
+            ? time.penalty.type === 'DNF' //This is because the time for inspection DNFs defaults to -1, and that would look weird
+                ? <p className="unselectable">DNF</p>
+                : <p className="unselectable">{`${millisecondsToSeconds(time.time)} (${time.penalty.type})`}</p>
+            : <p className="unselectable">{millisecondsToSeconds(time.time)}</p>
     }
 
-    const stopTimer = () => {
-        clearInterval(interval.current)
-        //Upload time
-    }
+    const renderTimer = () => { //render timer itself
+        switch(state) {
+            default:
+                return (<p className="unselectable">{times.length === 0 ? "0.00" : renderTime(times[times.length - 1])}</p>);
 
-    useEffect(() => {isActive ? startTimer() : stopTimer()}, [isActive])
+            case 'SPACE_PRESSED_INSPECTION':
+                return (<p className="unselectable" style={{color: "green"}}>15</p>);
+            case 'INSPECTION':
+                return <p className="unselectable">{inspectionTime}</p>;
+
+            case 'SPACE_PRESSED_TIMING':
+                return (<p className="unselectable" style={{color: "red"}}>0.00</p>);
+            case 'SPACE_PRESSED_VALID':
+                return (<p className="unselectable" style={{color: "green"}}>0.00</p>);
+            case 'STARTED':
+                return <p className="unselectable">{millisecondsToSeconds(time)}</p>
+        }
+    }
 
     return (
         <div>
-            <h1 style={{'fontSize': '10em', 'padding': '20px'}}><strong>{time.toFixed(2)}</strong></h1>
+            <h1 style={{'fontSize': '10em', 'padding': '20px'}}><strong>{renderTimer()}</strong></h1>
         </div>
     )
 }
