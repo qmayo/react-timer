92d7903096afc16a02428d9295fa7b5236ff116e
qmayo <qmayo2006@gmail.com>
2021-12-27 15:22:10 -0500

    Updated to work with 'src/components/utils/avgsAsCards.js'.

--- src/App.js
+++ src/App.js
@@ -10,13 +10,14 @@ import {useState} from 'react';
 
 function App() {
   const [eventName, setEvent] = useState('333')
+  const [avgsToDisplay, setAvgsToDisplay] = useState([{size: 5, type: 'avg'}, {size: 12, type: 'avg'}, {size: 50, type: 'avg'}]) //type: avg || mean
 
   return (
     <Router>
       <Navbar eventName={eventName} changeEvent={setEvent}/>
       <Switch>
         <Route path="/casual">
-          <Casual eventName={eventName}/>
+          <Casual eventName={eventName} avgsToDisplay={avgsToDisplay}/>
         </Route>
         <Route path="/competetive">
           <div className="Competetive">
--- src/components/Casual.js
+++ src/components/Casual.js
@@ -4,22 +4,11 @@ import Timer from './Timer'
 import Card from './Card'
 import Scramble from './scrambles/Scramble'
 
-import {getMean, getBestMoX, getAvg, getBestAoX} from './utils/sessionStatisticsTools'
-import {getTimes, getCurrentTimes} from './utils/storageTools';
+import avgsAsCards from './utils/avgsAsCards'
 
-
-const Casual = ({eventName}) => {
-    const [averageType, setAverageType] = useState('Ao5')
+const Casual = ({eventName, avgsToDisplay}) => {
     const [scrambleString, setScramble] = useState('')
     const [shouldScrambleUpdate, setShouldScrambleUpdate] = useState(false)
-
-    useEffect(() => {
-        if(['666wca', '777wca', '333ni', '444bld', '555bld'].includes(eventName)) {
-            setAverageType('Mo3')
-        } else {
-            setAverageType('Ao5')
-        }
-    }, [eventName])    
 
     return (
         <div>
@@ -32,24 +21,7 @@ const Casual = ({eventName}) => {
                         <Timer eventName={eventName} setShouldScrambleUpdate={setShouldScrambleUpdate} scrambleString={scrambleString}/>
                     </div>
                     <div className="is-flex is-flex-direction-row is-flex-wrap-wrap is-justify-content-space-around">
-                        <Card>
-                            <h4 className="title is-4">{averageType}:</h4>
-                            <p>Current: {}</p>
-                            <p>Best: 0</p>
-                            <p>Worst: 0</p>
-                        </Card>
-                        <Card>
-                            <h4 className="title is-4">Ao12:</h4>
-                            <p>Current: 0</p>
-                            <p>Best: 0</p>
-                            <p>Worst: 0</p>
-                        </Card>
-                        <Card>
-                            <h4 className="title is-4">Ao100:</h4>
-                            <p>Current: 0</p>
-                            <p>Best: 0</p>
-                            <p>Worst: 0</p>
-                        </Card>
+                        {avgsAsCards(avgsToDisplay, eventName)}
                     </div> 
                 </div>
             </div>
