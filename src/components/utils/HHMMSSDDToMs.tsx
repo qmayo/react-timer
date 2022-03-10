56d79164070d9c12413ccdb816b44e23340b891f
qmayo <69017882+qmayo@users.noreply.github.com>
2022-03-09 19:44:48 -0500

    Update HHMMSSDDToMs.tsx

--- src/components/utils/HHMMSSDDToMs.tsx
+++ src/components/utils/HHMMSSDDToMs.tsx
@@ -3,13 +3,9 @@ const HHMMSSDDToMs = (num: number): number => { //INPUT HH:MM.SS.DD
     const len = str.length;
   
     const decisecondsAsMs = Number(str.substring(str.length - 2, str.length)) * 10;
-    console.log(decisecondsAsMs)
     const secondsAsMs = Number(str.substring(str.length - 4, str.length - 2)) * 1000;
-    console.log(secondsAsMs)
     const minutesAsMs = Number(str.substring(str.length - 6, str.length - 4)) * 60000;
-    console.log(minutesAsMs)
     const hoursAsMs = Number(str.substring(str.length - 8, str.length - 6)) * 3600000;
-    console.log(hoursAsMs)
   
     return decisecondsAsMs + secondsAsMs + minutesAsMs + hoursAsMs
 }
